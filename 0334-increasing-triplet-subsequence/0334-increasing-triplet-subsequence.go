9a1f029517fd2f2f59d65994bd6959a5a99befac
Wongyu Lee <kyu21@outlook.com>
2022-10-12 23:15:04 +0900

    Time: 144 ms (47.55%), Space: 17.7 MB (89.51%) - LeetHub

---
+++ 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.go
@@ -0,0 +1,21 @@
+func increasingTriplet(nums []int) bool {
+    if len(nums) < 3 {
+        return false
+    }
+    
+    f, s := math.MaxInt, math.MaxInt
+    
+    // 2 1 5 0 4 6
+    // f f s f s t
+    for _, n := range nums {
+        if f >= n {
+            f = n
+        } else if s >= n {
+            s = n
+        } else {
+            return true
+        }
+    }
+    
+    return false
+}
