164a93095eaeee13f3bd102d37180d31b201d1ea
Wongyu Lee <kyu21@outlook.com>
2022-10-16 22:54:09 +0900

    Time: 335 ms (39.39%), Space: 7.5 MB (95.45%) - LeetHub

--- 1631-path-with-minimum-effort/1631-path-with-minimum-effort.go
+++ 1631-path-with-minimum-effort/1631-path-with-minimum-effort.go
@@ -1,61 +1,57 @@
 // 1. dijkstra + min heap
-// 2. MST, kruskal + unionfind
+// 2. MST, kruskal + unionfind + sort
 // 3. binary search..??
 
 func minimumEffortPath(heights [][]int) int {
+    // return minimalSpanningTree(heights)
     return dijkstraWithMinHeap(heights)
 }
 
 func dijkstraWithMinHeap(heights [][]int) int {
+    // min heap을 만든다
     n, m := len(heights), len(heights[0])
     h := &MinHeap{}
     heap.Init(h)
+    // 시작 0,0 을 push
     heap.Push(h, Item{0, 0, 0})
-    
+    // visit 배열
     visited := make([][]bool, n)
-    diff := make([][]int, n)
-    for i := range diff {
+    for i := range visited {
         visited[i] = make([]bool, m)
-        diff[i] = make([]int, m)
     }
-    
-    for i:=0; i<n; i++ {
-        for j:=0; j<m; j++ {
-            diff[i][j] = math.MaxInt
-        }
-    }
-    
-    visited[0][0] = true
-    diff[0][0] = 0
     
     dir := [][]int{{1,0},{-1,0},{0,1},{0,-1}}
     
+    // min heap 기준으로 돌면서
     for h.Len() > 0 {
         item := heap.Pop(h).(Item)
-        r, c := item.r, item.c
+        r, c, v := item.r, item.c, item.v
         
         visited[r][c] = true
-        diff[r][c] = item.v
         
+        // 맨 끝에 도달했으면 diff 값 반환
         if r == n-1 && c == m-1 {
-            return item.v
+            return v
         }
         
+        // 4 방향 확인
         for _, d := range dir {
             rr, cc := r + d[0], c + d[1]
-            if rr >= 0 && cc >= 0 && rr < n && cc < m  {
-                effort := abs(heights[rr][cc] - heights[r][c])
-                if effort < item.v {
-                    effort = item.v
-                }
-                if diff[rr][cc] > effort {
-                    heap.Push(h, Item{effort, rr, cc})
-                }
+            // 아직 방문하지 않았다면 크기 계산해서 heap에 push
+            if rr >= 0 && cc >= 0 && rr < n && cc < m && !visited[rr][cc] {
+                heap.Push(h, Item{max(v, abs(heights[rr][cc] - heights[r][c])), rr, cc})
             }
         }
     }
     
     return -1
+}
+
+func max(a, b int) int {
+    if a > b {
+        return a
+    }
+    return b
 }
 
 func abs(x int) int {
