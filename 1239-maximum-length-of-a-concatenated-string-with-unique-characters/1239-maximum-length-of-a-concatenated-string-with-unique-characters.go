a628b3f4d7574a717a24337b6d282543ac91243f
Wongyu Lee <kyu21@outlook.com>
2022-10-24 23:49:57 +0900

    Time: 85 ms (36.84%), Space: 7.6 MB (15.79%) - LeetHub

---
+++ 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.go
@@ -0,0 +1,34 @@
+import (
+	"fmt"
+)
+
+func maxLength(arr []string) int {
+    n := len(arr)
+    
+    var backTrack func(int, string) int
+    backTrack = func(index int, form string) int {
+        m := make(map[rune]bool, len(form))
+        for _, ch := range form {
+            if _, ok := m[ch]; ok {
+                return 0
+            }
+            m[ch] = true
+        }
+        
+        if index >= n {
+            return len(form)
+        }
+        
+        return max(backTrack(index+1, form), backTrack(index+1, form+arr[index]))
+        
+    }
+    
+    return backTrack(0, "")
+}
+
+func max(a, b int) int {
+    if a > b {
+        return a
+    }
+    return b
+}
