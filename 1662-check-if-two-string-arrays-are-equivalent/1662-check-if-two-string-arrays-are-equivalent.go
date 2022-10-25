6aab1814c5e84fd53ee82629b2ee6e77a9bdbde9
Wongyu Lee <kyu21@outlook.com>
2022-10-25 18:40:12 +0900

    Time: 0 ms (100.00%), Space: 2.2 MB (94.37%) - LeetHub

--- 1662-check-if-two-string-arrays-are-equivalent/1662-check-if-two-string-arrays-are-equivalent.go
+++ 1662-check-if-two-string-arrays-are-equivalent/1662-check-if-two-string-arrays-are-equivalent.go
@@ -1,13 +1,13 @@
 func arrayStringsAreEqual(word1 []string, word2 []string) bool {
-    w1, w2 := "", ""
-    
-    for _, str := range word1 {
-        w1 += str
+    return hash(word1) == hash(word2)
+}
+
+func hash(word []string) int {
+    hash := 5381
+    for _, str := range word {
+        for _, ch := range str {
+            hash = (hash * 33) ^ int(ch)
+        }
     }
-    
-    for _, str := range word2 {
-        w2 += str
-    }
-    
-    return w1 == w2
+    return hash
 }
