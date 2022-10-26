2ea009f091b2d70fd882c8d23d4258dbc0903219
Wongyu Lee <kyu21@outlook.com>
2022-10-26 23:22:31 +0900

    Time: 339 ms (48.72%), Space: 14.5 MB (5.13%) - LeetHub

---
+++ 0523-continuous-subarray-sum/0523-continuous-subarray-sum.go
@@ -0,0 +1,18 @@
+func checkSubarraySum(nums []int, k int) bool {
+    m := make(map[int]int, len(nums) + 1)
+    m[0] = -1
+    rem := 0
+    
+    for i, n := range nums {
+        rem = (rem + n) % k
+        if v, ok := m[rem]; ok {
+            if i - v >= 2 {
+                return true
+            }
+        } else {
+            m[rem] = i    
+        }    
+    }
+    
+    return false
+}
