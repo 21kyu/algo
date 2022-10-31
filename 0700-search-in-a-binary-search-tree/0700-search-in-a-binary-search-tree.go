13816b7e79f1030bee371196198a74bcd281c6f5
Wongyu Lee <kyu21@outlook.com>
2022-11-01 00:17:00 +0900

    Time: 50 ms (28.62%), Space: 7 MB (90.57%) - LeetHub

--- 0700-search-in-a-binary-search-tree/0700-search-in-a-binary-search-tree.go
+++ 0700-search-in-a-binary-search-tree/0700-search-in-a-binary-search-tree.go
@@ -1,11 +1,3 @@
-/**
- * Definition for a binary tree node.
- * type TreeNode struct {
- *     Val int
- *     Left *TreeNode
- *     Right *TreeNode
- * }
- */
 func searchBST(root *TreeNode, val int) *TreeNode {
     if root == nil {
         return nil
