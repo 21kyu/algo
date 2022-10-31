68f6b84cc98cdccf66383a05615609457ec4e6b1
Wongyu Lee <kyu21@outlook.com>
2022-11-01 00:19:50 +0900

    Time: 57 ms (7.07%), Space: 7.8 MB (8.08%) - LeetHub

--- 0700-search-in-a-binary-search-tree/0700-search-in-a-binary-search-tree.go
+++ 0700-search-in-a-binary-search-tree/0700-search-in-a-binary-search-tree.go
@@ -1,9 +1,11 @@
 func searchBST(root *TreeNode, val int) *TreeNode {
     if root == nil || root.Val == val {
         return root
-    } else if root.Val < val {
-        return searchBST(root.Right, val)
-    } else {
-        return searchBST(root.Left, val)
     }
+    
+    if root.Val < val {
+        return searchBST(root.Right, val)
+    }
+    
+    return searchBST(root.Left, val)
 }
