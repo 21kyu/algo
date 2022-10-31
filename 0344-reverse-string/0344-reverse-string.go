f4abc391ef05f9ca2ebf5db6bf27cbfbc8dae7de
Wongyu Lee <kyu21@outlook.com>
2022-10-31 22:38:36 +0900

    Time: 43 ms (76.47%), Space: 9.5 MB (5.69%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.go
+++ 0344-reverse-string/0344-reverse-string.go
@@ -1,12 +1,14 @@
 func reverseString(s []byte)  {
-    helper(s, 0, len(s)-1)
-}
-
-func helper(s []byte, l, r int) {
-    if l >= r {
-        return
+    
+    var helper func([]byte, int, int)
+    helper = func(s []byte, l, r int) {
+        if l >= r {
+            return
+        }
+    
+        s[l], s[r] = s[r], s[l]
+        helper(s, l+1, r-1)
     }
     
-    s[l], s[r] = s[r], s[l]
-    helper(s, l+1, r-1)
+    helper(s, 0, len(s)-1)
 }
