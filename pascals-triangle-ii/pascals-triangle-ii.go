e0f449ea1779eb4edb11d28c0c1aaf387ca3cec6
Wongyu Lee <kyu21@outlook.com>
2022-11-02 01:00:18 +0900

    Time: 3 ms, Memory: 2 MB - LeetHub

---
+++ pascals-triangle-ii/pascals-triangle-ii.go
@@ -0,0 +1,22 @@
+func getRow(rowIndex int) []int {
+    pascal := []int{1}
+    
+    var helper func(int, []int) []int
+    helper = func(i int, p []int) []int {
+        if i == rowIndex {
+            return p
+        }
+        
+        p = append(p, 1)
+        
+        for i:=len(p)-2; i>0; i-- {
+            p[i] += p[i-1]
+        }
+        
+        return helper(i+1, p)
+    }
+    
+    return helper(0, pascal)
+    
+}
+
