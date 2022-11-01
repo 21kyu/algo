0c2176cdd4e69927364055f2170b76a0be671334
Wongyu Lee <kyu21@outlook.com>
2022-11-01 22:16:27 +0900

    Time: 54 ms (18.63%), Space: 7 MB (34.31%) - LeetHub

---
+++ 1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.go
@@ -0,0 +1,60 @@
+func findBall(grid [][]int) []int {
+    m, n := len(grid), len(grid[0])
+    
+    var q []Ball
+    
+    visited := make([][]int, m)
+    for i := range visited {
+        visited[i] = make([]int, n)
+    }
+    
+    for i:=0; i<n; i++ {
+        q = append(q, Ball{i, 0, i})
+    }
+    
+    ans := make([]int, n)
+    for i := range ans {
+        ans[i] = -1
+    }
+    
+    for len(q) > 0 {
+        b := q[0]
+        q = q[1:]
+        
+        if b.r == m {
+            ans[b.s] = b.c
+            continue
+        }
+        
+        if visited[b.r][b.c] == 0 {
+            visited[b.r][b.c] = b.s + 1
+        } else {
+            ans[b.s] = visited[b.r][b.c] - 1
+            continue
+        }
+        
+        rr, cc := b.r + 1, 0
+        
+        if grid[b.r][b.c] == 1 {
+            if b.c+1 >= n || grid[b.r][b.c+1] != 1 {
+                continue
+            }
+            
+            cc = b.c + 1
+        } else {
+            if b.c-1 < 0 || grid[b.r][b.c-1] != -1 {
+                continue
+            }
+            
+            cc = b.c - 1
+        }
+        
+        q = append(q, Ball{b.s, rr, cc})
+    }
+    
+    return ans
+}
+
+type Ball struct {
+    s, r, c int
+}
